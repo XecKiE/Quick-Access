a550f6670d7ee7dfcc906893c759b29dfa715d7e
root <root@localhost.localdomain>
2022-05-21 17:35:54 +0200

    des trucs pas commits ?

---
+++ index.php
@@ -0,0 +1,6 @@
+<h1>Extension chrome "Quick Access"</h1>
+<p>Cette simple extension permet un accès rapide aux favoris de Chrome en utilisant un raccourci et une fuzzy search.</p>
+<ul>
+	<li><a href="https://github.com/XecKiE/Quick-Access">GitHub</a></li>
+	<li><a href="https://chrome.google.com/webstore/detail/quick-access/iihnbnncigiiampnmeiiaiomcbnjmgmb">Chrome store</a></li>
+</ul>
